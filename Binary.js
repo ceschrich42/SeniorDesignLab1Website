da4807225cbdaaf01c31a696fafe24002a9f25a6
Joe Biggins <joseph-biggins@uiowa.edu>
2018-09-20 17:19:03 -0500

    Merge branch 'master' of https://github.com/ceschrich42/SeniorDesignLab1Website
    # Conflicts:
    #	package-lock.json

--- Binary.js
+++ Binary.js
@@ -1,6 +1,29 @@
 const five = require('johnny-five');
-//var board = new five.Board();
+var board = new five.Board();
 
+board.on("ready", function () {
+    var led7 = new five.Led(7);
+    var led8 = new five.Led(8);
+    var led9 = new five.Led(9);
+    var led10 = new five.Led(10);
+    var led11 = new five.Led(11);
+    var led12 = new five.Led(12);
+    var led13 = new five.Led(13);
+
+    bin = twosComplement(-3,7);
+    bin = bin.split('');
+    console.log(bin);
+    this.repl.inject({
+        led7: led7,
+        led8: led8,
+        led9: led9,
+        led10: led10,
+        led11: led11,
+        led12: led12,
+        led13: led13
+    });
+    turnOnLeds(bin);
+});
 function turnOnLeds(bin){
     var led7 = new five.Led(7);
     var led8 = new five.Led(8);
@@ -77,10 +100,10 @@ function turnOnLeds(bin){
 
 //https://gist.github.com/bsara/519df5f91833d01c20ec
 function twosComplement(value, bitCount) {
-    let binaryStr;
+    var binaryStr;
 
     if (value >= 0) {
-        let twosComp = value.toString(2);
+        var twosComp = value.toString(2);
         binaryStr    = padAndChop(twosComp, '0', (bitCount || twosComp.length));
     } else {
         binaryStr = (Math.pow(2, bitCount) + value).toString(2);
@@ -89,11 +112,9 @@ function twosComplement(value, bitCount) {
             return undefined
         }
     }
-    return `${binaryStr}`;
+    return binaryStr;
 }
 
 function padAndChop(str, padChar, length) {
     return (Array(length).fill(padChar).join('') + str).slice(length * -1);
 }
-
-console.log(twosComplement(3,7));
